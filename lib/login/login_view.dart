9089409788625f33e717ced826ae4fabe96c8571
Niklas D <80592781+Niklas-gif@users.noreply.github.com>
2024-03-27 17:01:46 +0100

    Testwidget & base64encoding demo

--- lib/login/login_view.dart
+++ lib/login/login_view.dart
@@ -1,11 +1,48 @@
-
 import 'package:flutter/material.dart';
+import 'package:vroom_campus_app/login/login_vm.dart';
+
+//Run this for testing the widget we kick this out later ;)
+void main() {
+  runApp(const TestWidget());
+}
+
+class TestWidget extends StatelessWidget {
+  const TestWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        useMaterial3: true,
+      ),
+      home: const LoginView(),
+    );
+  }
+}
+//
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(),
+            TextField(),
+            ElevatedButton(
+                onPressed: () => {
+                  LoginVM().decodeBase64("Bob", "Password1")
+                },
+                child: const Text("Login"))
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/login/login_vm.dart
+++ lib/login/login_vm.dart
@@ -3,11 +3,13 @@ import 'package:flutter/cupertino.dart';
 import 'dart:convert';
 
 class LoginVM extends ChangeNotifier {
-  String _userName = "";
-  String _password = "";
+  //String _userName = "";
+  //String _password = "";
 
-  void decodeBase64() {
-
+  void decodeBase64(String username,String password) {
+    Codec<String,String> authBase64 = utf8.fuse(base64);
+    String encoded = authBase64.encode('$username:$password');
+    print(encoded);
   }
 
 }
