1812a84e0ac677ae1da426f8451f102125f1a6ca
Niklas D <80592781+Niklas-gif@users.noreply.github.com>
2024-03-27 16:35:20 +0100

    login & auth wip

---
+++ lib/login/login_vm.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+
+class LoginVM extends ChangeNotifier {
+  String _userName = "";
+  String _password = "";
+
+  void decodeBase64() {
+
+  }
+
+}
