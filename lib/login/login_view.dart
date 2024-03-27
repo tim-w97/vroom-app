92fed6a414188d382c663e185cdeac9b8653f4af
Niklas D <80592781+Niklas-gif@users.noreply.github.com>
2024-03-27 19:24:40 +0100

    provider wip

--- lib/login/login_view.dart
+++ lib/login/login_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vroom_campus_app/login/login_vm.dart';
+import 'package:provider/provider.dart';
 
 //Run this for testing the widget we kick this out later ;)
 void main() {
@@ -28,19 +29,27 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(),
-            TextField(),
-            ElevatedButton(
-                onPressed: () => {
-                  LoginVM().decodeBase64("Bob", "Password1")
-                },
-                child: const Text("Login"))
-          ],
+    return ChangeNotifierProvider(
+      create: (context) => LoginVM(),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text("Login Test Widget"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Decode"),
+              TextField(),
+              TextField(),
+              ElevatedButton(
+                  onPressed: () => {
+                    LoginVM().decodeBase64("Bob", "Password1")
+                  },
+                  child: const Text("Login"))
+            ],
+          ),
         ),
       ),
     );
--- lib/login/login_vm.dart
+++ lib/login/login_vm.dart
@@ -3,13 +3,17 @@ import 'package:flutter/cupertino.dart';
 import 'dart:convert';
 
 class LoginVM extends ChangeNotifier {
-  //String _userName = "";
-  //String _password = "";
+
+  String _decodedString = "";
+
+  String get decodedString {
+    return _decodedString;
+  }
 
   void decodeBase64(String username,String password) {
     Codec<String,String> authBase64 = utf8.fuse(base64);
-    String encoded = authBase64.encode('$username:$password');
-    print(encoded);
+    _decodedString = authBase64.encode('$username:$password');
+    notifyListeners();
   }
 
 }
