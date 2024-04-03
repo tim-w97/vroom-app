0253cb244867d650cfd65ee9a2f115930d16e879
ndemel <niklas.demel@hof-university.de>
2024-04-03 13:54:33 +0200

    wip persistence

--- lib/login/login_vm.dart
+++ lib/login/login_vm.dart
@@ -2,8 +2,10 @@
 import 'package:flutter/cupertino.dart';
 import 'dart:convert';
 
-class LoginVM extends ChangeNotifier {
+import 'package:shared_preferences/shared_preferences.dart';
 
+class LoginVM extends ChangeNotifier {
+  late final SharedPreferences sharedPreferences;
   String _decodedString = "";
 
   String get decodedString {
@@ -13,6 +15,14 @@ class LoginVM extends ChangeNotifier {
   void decodeBase64(String username,String password) {
     Codec<String,String> authBase64 = utf8.fuse(base64);
     _decodedString = authBase64.encode('$username:$password');
+    save();
+    notifyListeners();
+  }
+
+  Future<void> save() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.setString("Base64Auth", _decodedString);
+    print(sharedPreferences.get("Base64Auth"));
     notifyListeners();
   }
 
