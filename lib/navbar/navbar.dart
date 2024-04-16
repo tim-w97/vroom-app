aca363d9e4b997fd2fa3c6f0545839975115f1dc
ndemel <niklas.demel@hof-university.de>
2024-04-16 10:46:32 +0200

    Moved Provider from Testwidget to Bottomnavbar

--- lib/navbar/navbar.dart
+++ lib/navbar/navbar.dart
@@ -9,11 +9,8 @@ class TestWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => NavigationVM(),
-      child: const MaterialApp(
-        home: BottomNavBar(),
-      ),
+    return const MaterialApp(
+      home: BottomNavBar(),
     );
   }
 }
@@ -23,26 +20,29 @@ class BottomNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: Consumer<NavigationVM>(
-        builder: (context, provider, child) => provider.currentPage,
-      ),
-      bottomNavigationBar: Consumer<NavigationVM>(
-        builder: (context, provider, child) {
-          return BottomNavigationBar(
-            currentIndex: provider.currentIndex,
-            onTap: provider.setIndex,
-            items: provider.items
-                .map((item) => BottomNavigationBarItem(
-                      icon: Icon(item.icon),
-                      label: item.label,
-                    ))
-                .toList(),
-          );
-        },
+    return ChangeNotifierProvider(
+      create: (_) => NavigationVM(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home'),
+        ),
+        body: Consumer<NavigationVM>(
+          builder: (context, provider, child) => provider.currentPage,
+        ),
+        bottomNavigationBar: Consumer<NavigationVM>(
+          builder: (context, provider, child) {
+            return BottomNavigationBar(
+              currentIndex: provider.currentIndex,
+              onTap: provider.setIndex,
+              items: provider.items
+                  .map((item) => BottomNavigationBarItem(
+                        icon: Icon(item.icon),
+                        label: item.label,
+                      ))
+                  .toList(),
+            );
+          },
+        ),
       ),
     );
   }
