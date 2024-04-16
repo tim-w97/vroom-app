5b3292097acee04be51fd5da3bcf87ca50e20fd9
ndemel <niklas.demel@hof-university.de>
2024-04-16 12:09:57 +0200

    moved logic from vm to view

--- lib/authentication/authentication_view.dart
+++ lib/authentication/authentication_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:vroom_campus_app/authentication/authentication_vm.dart';
+import 'package:vroom_campus_app/authentication/login/login_view.dart';
+import 'package:vroom_campus_app/authentication/register/register_view.dart';
 
 //Run this for testing the widget we kick this out later ;)
 void main() {
@@ -26,14 +28,22 @@ class TestWidget extends StatelessWidget {
 class AuthenticationView extends StatelessWidget {
   const AuthenticationView({super.key});
 
+  StatelessWidget toggleWitched(AuthenticationVM vm) {
+    switch(vm.currentScreen) {
+      case AuthenticationType.login:
+        return const LoginView();
+      case AuthenticationType.register:
+        return const RegisterView();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    //TODO switch between login and register
+
     return ChangeNotifierProvider(
       create: (context)=> AuthenticationVM(),
       child: Consumer<AuthenticationVM>(builder: (context, vm, child) {
-        return vm.currentSelection;
+        return toggleWitched(vm);
       }),
     );
   }
--- lib/authentication/authentication_vm.dart
+++ lib/authentication/authentication_vm.dart
@@ -1,7 +1,5 @@
 
 import 'package:flutter/cupertino.dart';
-import 'package:vroom_campus_app/authentication/login/login_view.dart';
-import 'package:vroom_campus_app/authentication/register/register_view.dart';
 
 enum AuthenticationType {
   login,
@@ -9,26 +7,10 @@ enum AuthenticationType {
 }
 
 class AuthenticationVM extends ChangeNotifier {
-StatelessWidget _currentSelection = const LoginView();
 AuthenticationType _currentScreen = AuthenticationType.login;
 
 AuthenticationType get currentScreen {
   return _currentScreen;
-}
-
-StatelessWidget get currentSelection {
-  return _currentSelection;
-}
-
-void _setScreen() {
-  switch(_currentScreen) {
-    case AuthenticationType.login:
-      _currentSelection = const LoginView();
-      break;
-    case AuthenticationType.register:
-      _currentSelection = const RegisterView();
-      break;
-  }
 }
 
 void toggleScreen() {
@@ -37,7 +19,6 @@ void toggleScreen() {
   } else {
     _currentScreen = AuthenticationType.login;
   }
-  _setScreen();
   notifyListeners();
 }
 }
