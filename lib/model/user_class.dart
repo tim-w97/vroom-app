4a1c51f26383eae0eb03833d9c450ec17a084d56
ndemel <niklas.demel@hof-university.de>
2024-04-17 10:38:11 +0200

    userclass wip

---
+++ lib/model/user_class.dart
@@ -0,0 +1,17 @@
+import 'gender.dart';
+
+class User {
+  //Requierd
+  String _firstName = "";
+  String _surName = "";
+  String _email = "";
+  String _password = "";
+  int _evaluation = 0;
+
+  //Optional
+  String ?_phoneNumber;
+  Gender ?_gender;
+  String ?_description;
+
+  //TODO constructor
+}
