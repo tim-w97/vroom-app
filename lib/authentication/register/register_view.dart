907ff75b922f3eaed3a7a50177bb175144e5961d
ndemel <niklas.demel@hof-university.de>
2024-04-24 08:12:18 +0200

    Fixed layout

--- lib/authentication/register/register_view.dart
+++ lib/authentication/register/register_view.dart
@@ -13,81 +13,84 @@ class RegisterView extends StatelessWidget {
         create: (context) => RegisterVM(),
         child: Scaffold(
           body: Center(
-              child: Column(
-            children: [
-              Consumer<AuthenticationVM>(
-                builder: (context, vm, child) {
-                  return ElevatedButton(
-                      onPressed: () {
-                        vm.toggleScreen();
-                      },
-                      child: const Text("Switch to login"));
-                },
-              ),
-              Consumer<RegisterVM>(
-                builder: (context, vm, child) {
-                  return Column(
-                    children: [
-                      const Padding(padding: EdgeInsets.all(10)),
-                      SizedBox(
-                        width: 300,
-                        child: TextField(
-                          onChanged: (value) {
-                            vm.setUsername(value);
-                          },
-                          decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Username'),
+              child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Consumer<AuthenticationVM>(
+                  builder: (context, vm, child) {
+                    return ElevatedButton(
+                        onPressed: () {
+                          vm.toggleScreen();
+                        },
+                        child: const Text("Switch to login"));
+                  },
+                ),
+                Consumer<RegisterVM>(
+                  builder: (context, vm, child) {
+                    return Column(
+                      children: [
+                        const Padding(padding: EdgeInsets.all(10)),
+                        SizedBox(
+                          width: 300,
+                          child: TextField(
+                            onChanged: (value) {
+                              vm.setUsername(value);
+                            },
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Username'),
+                          ),
                         ),
-                      ),
-                      const Padding(padding: EdgeInsets.all(10)),
-                      SizedBox(
-                        width: 300,
-                        child: TextField(
-                          onChanged: (value) {
-                            vm.setEmail(value);
-                          },
-                          decoration: const InputDecoration(
-                              border: OutlineInputBorder(), labelText: 'Email'),
+                        const Padding(padding: EdgeInsets.all(10)),
+                        SizedBox(
+                          width: 300,
+                          child: TextField(
+                            onChanged: (value) {
+                              vm.setEmail(value);
+                            },
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Email'),
+                          ),
                         ),
-                      ),
-                      const Padding(padding: EdgeInsets.all(10)),
-                      SizedBox(
-                        width: 300,
-                        child: TextField(
-                          onChanged: (value) {
-                            vm.setPassword(value);
-                          },
-                          obscureText: true,
-                          decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Password'),
+                        const Padding(padding: EdgeInsets.all(10)),
+                        SizedBox(
+                          width: 300,
+                          child: TextField(
+                            onChanged: (value) {
+                              vm.setPassword(value);
+                            },
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Password'),
+                          ),
                         ),
-                      ),
-                      const Padding(padding: EdgeInsets.all(10)),
-                      SizedBox(
-                        width: 300,
-                        child: TextField(
-                          onChanged: (value) {
-                            vm.validatePassword(value);
-                          },
-                          obscureText: true,
-                          decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Repeat Password'),
+                        const Padding(padding: EdgeInsets.all(10)),
+                        SizedBox(
+                          width: 300,
+                          child: TextField(
+                            onChanged: (value) {
+                              vm.validatePassword(value);
+                            },
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Repeat Password'),
+                          ),
                         ),
-                      ),
-                      const Padding(padding: EdgeInsets.all(10)),
-                      ElevatedButton(
-                          onPressed: () {
-                            //TODO Submit Registration
-                          },
-                          child: const Text("Register"))
-                    ],
-                  );
-                },
-              )
-            ],
+                        const Padding(padding: EdgeInsets.all(10)),
+                        ElevatedButton(
+                            onPressed: () {
+                              //TODO Submit Registration
+                            },
+                            child: const Text("Register"))
+                      ],
+                    );
+                  },
+                )
+              ],
+            ),
           )),
         ));
   }
