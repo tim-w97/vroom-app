a4047b1d1f7c4c53c6570453517e7b09aa9d3db2
ndemel <niklas.demel@hof-university.de>
2024-04-24 10:04:58 +0200

    Car class

--- lib/model/car.dart
+++ lib/model/car.dart
@@ -1,4 +1,17 @@
 
+import 'dart:html';
+
 class Car {
+  String licensePlate;
+  String model;
+  String color;
+  Url imageURL;
+  
+  Car({
+    required this.licensePlate,
+    required this.model,
+    required this.color,
+    required this.imageURL
+});
 
 }
