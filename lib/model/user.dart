ad2ca2d4f6038926fba68050639103f876d9fdc2
ndemel <niklas.demel@hof-university.de>
2024-04-24 09:31:03 +0200

    constructor

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -4,17 +4,32 @@ import 'gender.dart';
 
 class User {
   //Required
-  String _firstName = "";
-  String _surName = "";
-  String _email = "";
-  String _password = "";
-  int _evaluation = 0;
+  String firstName = "";
+  String surName = "";
+  String email = "";
+  String password = "";
 
   //Optional
-  String ?_phoneNumber;
-  Gender ?_gender;
-  String ?_description;
-  Image ? _image; //TODO Default picture when null
+  String? phoneNumber;
+  Gender? gender;
+  String? description;
+  int? evaluation;
+  Image? image; //TODO Default picture when null
+  List<String>? favoriteDrivers;
+  List<String>? rememberedRides;
 
-  //TODO constructor
+  User({
+    required this.firstName,
+    required this.surName,
+    required this.email,
+    required this.password,
+    this.phoneNumber,
+    this.gender,
+    this.description,
+    this.evaluation,
+    this.image,
+    this.favoriteDrivers,
+    this.rememberedRides,
+  });
+  
 }
