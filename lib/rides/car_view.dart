a8ac292ec23587c094427819b642437c992b316a
ndemel <niklas.demel@hof-university.de>
2024-05-15 10:56:57 +0200

    reformating

--- lib/rides/car_view.dart
+++ lib/rides/car_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vroom_campus_app/rides/map_view.dart';
 
 class CarView extends StatelessWidget {
   const CarView({super.key});
@@ -10,17 +11,23 @@ class CarView extends StatelessWidget {
         children: [
           const Text("Mit welchem Auto fährst du?"),
           const Padding(padding: EdgeInsets.all(10)),
+          Image.asset('lib/assets/images/auto.png'),
+          const Padding(padding: EdgeInsets.all(10)),
           ElevatedButton(
               onPressed: () {
                 print("+");
               },
               child: const Text("+ Neues Fahrzeug")),
-        Image.asset('lib/assets/images/auto.png'),
           const Padding(padding: EdgeInsets.all(10)),
-          FloatingActionButton(onPressed: (){},child: const Icon(Icons.arrow_forward))
+          FloatingActionButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const MapView()),
+                );
+              },
+              child: const Icon(Icons.arrow_forward)),
         ],
       ),
-
     );
   }
 }
