6c003539e02ebc5a62afff9d05c80eaba89fe4a2
ndemel <niklas.demel@hof-university.de>
2024-05-15 10:33:17 +0200

    Car placeholder

--- lib/rides/car_view.dart
+++ lib/rides/car_view.dart
@@ -9,13 +9,18 @@ class CarView extends StatelessWidget {
       child: Column(
         children: [
           const Text("Mit welchem Auto fährst du?"),
+          const Padding(padding: EdgeInsets.all(10)),
           ElevatedButton(
               onPressed: () {
                 print("+");
               },
-              child: const Text("+ Neues Fahrzeug"))
+              child: const Text("+ Neues Fahrzeug")),
+        Image.asset('lib/assets/images/auto.png'),
+          const Padding(padding: EdgeInsets.all(10)),
+          FloatingActionButton(onPressed: (){},child: const Icon(Icons.arrow_forward))
         ],
       ),
+
     );
   }
 }
