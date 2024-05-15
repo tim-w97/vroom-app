0af5f67ef29eaed954a73e86b983db63bd791e1b
btolgurov <boris.tolgurov@hof-university.de>
2024-05-15 12:06:33 +0200

    added connection to backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,10 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:vroom_campus_app/style/dark_theme.dart';
-import 'package:vroom_campus_app/style/light_theme.dart';
 import 'package:vroom_campus_app/userdefaults/shared_preferences_helper.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-import 'navbar/navbar.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await EasyLocalization.ensureInitialized();
-
-  runApp(
-    EasyLocalization(
-      supportedLocales: const [Locale('en'), Locale('de')],
-      path: 'lib/assets/intl',
-      fallbackLocale: const Locale('en'),
-      useOnlyLangCode: true,
-      child: const MyApp(),
-    ),
-  );
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -28,13 +14,79 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: getLightTheme(),
-        localizationsDelegates: context.localizationDelegates,
-        supportedLocales: context.supportedLocales,
-        locale: context.locale,
-        home: const BottomNavBar()
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'VRoom Campus'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      fetchItems();
+      _counter++;
+    });
+  }
+
+  Future<void> fetchItems() async {
+    final response = await http.get(Uri.parse('http://10.0.2.2:8080/rides'));
+
+    if (response.statusCode == 200) {
+      // Die Anfrage war erfolgreich
+      var data = json.decode(response.body);
+      print("succes");
+      //print(data);
+    } else {
+      // Die Anfrage war nicht erfolgreich
+      print("not success");
+     // throw Exception('Failed to load items');
+
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
