13aca79a461703a9ec25ca6ebde28757b736a071
ndemel <niklas.demel@hof-university.de>
2024-05-22 09:10:50 +0200

    removed comment

--- lib/rides/ride_vm.dart
+++ lib/rides/ride_vm.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:open_route_service/open_route_service.dart';
-//import 'package:vroom_campus_app/passwd.dart';
+import 'package:vroom_campus_app/passwd.dart';
 
 class RideVM extends ChangeNotifier {
-  /*OpenRouteService openRouteService =
-      OpenRouteService(apiKey: AppConfigs.API_KEY);*/
+  OpenRouteService openRouteService =
+      OpenRouteService(apiKey: AppConfigs.API_KEY);
 
   void test() {}
 }
