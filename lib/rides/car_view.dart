5eb33c64ecd05537b88dfaed833e27098c62519d
ndemel <niklas.demel@hof-university.de>
2024-05-22 07:23:37 +0200

    Layout

--- lib/rides/car_view.dart
+++ lib/rides/car_view.dart
@@ -6,34 +6,39 @@ class CarView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          const Text("Mit welchem Auto fährst du?"),
-          const Padding(padding: EdgeInsets.all(10)),
-          Image.asset('lib/assets/images/auto.png'),
-          const Padding(padding: EdgeInsets.all(10)),
-          ElevatedButton(
-              onPressed: () {
-                print("+");
-              },
-              child: const Text("+ Neues Fahrzeug")),
-          const Padding(padding: EdgeInsets.all(10)),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: FloatingActionButton(
+    return Stack(
+      children: [
+        Center(
+          child: Column(
+            children: [
+              const Text("Mit welchem Auto fährst du?"),
+              const Padding(padding: EdgeInsets.all(10)),
+              Image.asset('lib/assets/images/auto.png'),
+              const Padding(padding: EdgeInsets.all(10)),
+              ElevatedButton(
                   onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => const MapView()),
-                    );
+                    print("+");
                   },
-                  child: const Icon(Icons.arrow_forward)),
-            ),
+                  child: const Text("+ Neues Fahrzeug")),
+              const Padding(padding: EdgeInsets.all(10)),
+            ],
           ),
-        ],
-      ),
+        ),
+        Align(
+          alignment: Alignment.bottomRight,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FloatingActionButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const MapView()),
+                  );
+                },
+                child: const Icon(Icons.arrow_forward)),
+          ),
+        ),
+      ],
     );
   }
 }
