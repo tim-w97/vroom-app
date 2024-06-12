761efb418a8ec39d8024c36090720f9a024520ac
ndemel <niklas.demel@hof-university.de>
2024-06-12 13:46:46 +0200

    wip getuserbyid

--- lib/authentication/login/login_vm.dart
+++ lib/authentication/login/login_vm.dart
@@ -3,6 +3,9 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import '../../model/user.dart';
+import '../../userdata.dart';
+
 class LoginVM extends ChangeNotifier {
   String _username = "";
   String _password = "";
@@ -29,17 +32,37 @@ class LoginVM extends ChangeNotifier {
         'authorization': basicAuth,
       },
     );
-    print("okay");
 
-    if (response.statusCode == 201) {
+    if (response.statusCode == 200) {
       final responseJson = jsonDecode(response.body);
       print(response.statusCode);
       print("Login successfull");
       notifyListeners();
     } else {
+      print(response.statusCode);
       print("Login not successfull");
       print(response.statusCode);
       throw Exception('Failed to login');
+    }
+  }
+
+  Future<void> getUserByID() async { //getUserByID
+    //localhost:8080/user/666982c25f80fbef281ec783
+    final String basicAuth = 'Basic ' + base64Encode(utf8.encode('$_username:$_password'));
+    final response = await http.get(
+      Uri.parse('$API_URL/user/666982c25f80fbef281ec783'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'authorization': basicAuth,
+      },
+    );
+    if (response.statusCode == 200) {
+      print(response.statusCode);
+      Map<String,dynamic> jsonMap = jsonDecode(response.body);
+      UserDataModel.sharedInstance.userContainer.surName = jsonMap['firstName'];
+      notifyListeners();
+    } else {
+      print(response.statusCode);
     }
   }
 
