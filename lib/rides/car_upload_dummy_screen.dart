8ebc2efcdab45209454f622395904449f4d8da40
Tim <timwagner997@gmail.com>
2024-06-19 08:59:29 +0200

    creaded dummy screen for car image upload

---
+++ lib/rides/car_upload_dummy_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CarUploadDummyScreen extends StatelessWidget {
+  const CarUploadDummyScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Test - Bild von Auto hochladen'),
+      ),
+      body: ,
+    );
+  }
+}
